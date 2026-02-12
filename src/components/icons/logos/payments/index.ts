4b5b255e59857afe4d87f94b6dd44fc4b966e4d6
Makc240305 <maxim240305@gmail.com>
2026-02-12 18:30:53 +0100

    redesign product page

---
+++ src/components/icons/logos/payments/index.ts
@@ -0,0 +1,7 @@
+import ApplePayLogo from '@/components/icons/pay/Apple_Pay_logo.svg'
+import GooglePayLogo from '@/components/icons/pay/Google_Pay_Logo.svg'
+import MastercardLogo from '@/components/icons/pay/Mastercard-logo.svg'
+import Privat24Logo from '@/components/icons/pay/Privat24_Logo.png'
+import VisaLogo from '@/components/icons/pay/Visa_2021.svg'
+
+export { ApplePayLogo, GooglePayLogo, MastercardLogo, Privat24Logo, VisaLogo }
--- src/components/product/Gallery.tsx
+++ src/components/product/Gallery.tsx
@@ -20,8 +20,19 @@ export const Gallery: React.FC<Props> = ({ gallery }) => {
   const [api, setApi] = React.useState<CarouselApi>()
 
   useEffect(() => {
-    if (!api) {
-      return
+    if (!api) return
+
+    const onSelect = () => {
+      setCurrent(api.selectedScrollSnap())
+    }
+
+    onSelect()
+    api.on('select', onSelect)
+    api.on('reInit', onSelect)
+
+    return () => {
+      api.off('select', onSelect)
+      api.off('reInit', onSelect)
     }
   }, [api])
 
@@ -49,31 +60,43 @@ export const Gallery: React.FC<Props> = ({ gallery }) => {
 
   return (
     <div>
-      <div className="relative w-full overflow-hidden mb-8">
-        <Media
-          resource={gallery[current].image}
-          className="w-full"
-          imgClassName="w-full rounded-lg"
-        />
-      </div>
-
-      <Carousel setApi={setApi} className="w-full" opts={{ align: 'start', loop: false }}>
-        <CarouselContent>
+      <Carousel
+        setApi={setApi}
+        className="w-full touch-pan-y"
+        opts={{ align: 'start', loop: false }}
+      >
+        <CarouselContent className="ml-0">
           {gallery.map((item, i) => {
             if (typeof item.image !== 'object') return null
 
             return (
-              <CarouselItem
-                className="basis-1/5"
-                key={`${item.image.id}-${i}`}
-                onClick={() => setCurrent(i)}
-              >
-                <GridTileImage active={i === current} media={item.image} />
+              <CarouselItem className="basis-full pl-0" key={`${item.image.id}-${i}`}>
+                <div className="relative w-full overflow-hidden mb-8">
+                  <Media resource={item.image} className="w-full" imgClassName="w-full rounded-lg" />
+                </div>
               </CarouselItem>
             )
           })}
         </CarouselContent>
       </Carousel>
+
+      <div className="flex items-center gap-3 overflow-x-auto">
+        {gallery.map((item, i) => {
+          if (typeof item.image !== 'object') return null
+
+          return (
+            <button
+              key={`${item.image.id}-thumb-${i}`}
+              type="button"
+              className="shrink-0 basis-1/5"
+              onClick={() => api?.scrollTo(i, true)}
+              aria-label={`Go to image ${i + 1}`}
+            >
+              <GridTileImage active={i === current} media={item.image} />
+            </button>
+          )
+        })}
+      </div>
     </div>
   )
 }
